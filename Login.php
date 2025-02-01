59e247db038331a00602da44c4230f75f74dd5d8
Matin-Karshenas <karshenasmtyn@gmail.com>
2025-02-01 20:29:48 +0330

    commit

--- Login.php
+++ Login.php
@@ -28,9 +28,13 @@
 
     if($data)
     {
+        $query = "SELECT `Name` FROM `website` WHERE `Email` = '$Email'";
+        $result = mysqli_query($data_sa, $query);
         echo("You are logged");
         $Action = "Logged";
-        header("Location: Trump.html?cameFromLogin=true&actionw=".$Action."#contact");
+        $Row = mysqli_fetch_assoc($result);
+        $NameOf_Client = $Row["Name"];
+        header("Location: Trump.html?cameFromLogin=true&actionw=".$Action."&NAME=".$NameOf_Client."#contact");
         exit();
     }
     else
